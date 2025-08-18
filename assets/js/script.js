9a0e61868390cdbac74224ab33e54a74fc10e186
Gameringly <luke.x.x.williams@gmail.com>
2025-08-18 15:26:23 +0100

    Revert "add link to site at the top of the readme"
    This reverts commit 26ef2e57bb550160f7a5b52583867e3ac2abce4b.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 let gridSize = 4
-let currentColor = "red";
 
 const grid = document.getElementById("grid");
-const palette = document.getElementById("palette");
-
-// Get colour from palette  
-palette.addEventListener("change", () => {
-  const checked = palette.querySelector("input:checked");
-  if (checked) currentColor = checked.value;
-});
 
 // Build blank grid
 function buildGrid() {
@@ -17,7 +9,7 @@ function buildGrid() {
         const px = document.createElement("div");
         px.className = "pixel";
         px.addEventListener("click",()=> {
-            px.style.backgroundColor = currentColor;
+            px.style.backgroundColor = "red";
         });
         grid.appendChild(px);
      }
