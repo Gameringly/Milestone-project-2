e34aba55342d87df4c8cc7f408c04c7e3e80f7ba
Gameringly <luke.x.x.williams@gmail.com>
2025-08-18 15:02:51 +0100

    create 4 by 4 tile grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+let gridSize = 4
+
 const grid = document.getElementById("grid");
 
 // Build blank grid
 function buildGrid() {
-    const px = document.createElement("div");
-    px.className = "pixel";
-    grid.appendChild(px);
+     grid.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
+     for (let i=0; i < gridSize * gridSize; i++) {
+        const px = document.createElement("div");
+        px.className = "pixel";
+        grid.appendChild(px);
+     }
 }
 
 buildGrid();
