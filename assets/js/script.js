5e966c07b9ff90eeb412f747a8c8b50eea445d3d
Gameringly <luke.x.x.williams@gmail.com>
2025-08-21 15:27:27 +0100

    add generate solution function which has its result printed to the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let gridSize = 4
+let solutionGrid = []
 let currentColor = "red";
 
+const colorValues = ["red","green","blue","yellow","orange","purple"];
 const grid = document.getElementById("grid");
 const palette = document.getElementById("palette");
 
@@ -23,9 +25,19 @@ function buildGrid() {
      }
 }
 
+// Generate random solution
+function generateSolution() {
+  solutionGrid = [];
+  for (let i=0;i<gridSize*gridSize;i++) {
+    solutionGrid.push(colorValues[Math.floor(Math.random()*colorValues.length)]);
+  }
+}
+
 // Start new game
 function startGame() {
   console.log("game start")
+  generateSolution();
+  console.log(solutionGrid)
 }
 
 buildGrid();
